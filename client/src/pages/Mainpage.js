1f6631fca4799a8be73b89ec5f91d40befad296f
xbh0403 <xbh0403@gmail.com>
2022-07-15 19:00:39 -0700

    Responsive nav bar

--- client/src/pages/Mainpage.js
+++ client/src/pages/Mainpage.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import Navbar_2 from '../components/Navbar_2'
 
 function Mainpage() {
   return (
-    // <div><Navbar/></div>
-    <div><Navbar_2/></div>
+    <div><Navbar/></div>
   )
 }
 
