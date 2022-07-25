78c9eb608dc689799d39553466b440781234a679
xbh0403 <xbh0403@gmail.com>
2022-07-24 23:00:11 -0700

    new structure

--- app.js
+++ app.js
@@ -12,9 +12,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 module.exports = app;
